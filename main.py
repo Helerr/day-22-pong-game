07c0201d8aa4ec8c5b815bde223c65c6e63d2376
alexandru.neculai <alleexx250595@gmail.com>
2022-03-15 15:32:13 +0200

    ball implemented - currently moves only to top-right corner

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 import time
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
-paddle = Paddle()
-
-screen.update()
-time.sleep(0.01)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.03)
+    screen.update()
+    ball.move()
+
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,20 +1,19 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.penup()
-        self.resizemode("user")
-        self.speed("fastest")
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(350, 0)
+        self.goto(position)
 
     def up(self):
-        if self.ycor() < 250:
-            self.goto(350, y=self.ycor() + 20)
+        if self.ycor() < 230:
+            self.goto(self.xcor(), y=self.ycor() + 20)
 
     def down(self):
-        if self.ycor() > - 250:
-            self.goto(350, y=self.ycor() - 20)
+        if self.ycor() > - 230:
+            self.goto(self.xcor(), y=self.ycor() - 20)
